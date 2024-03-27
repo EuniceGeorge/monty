1a4dec4cd235c4bc3acda9b7bf81d97b15157f90
Eunice George <georgeunice408@gmail.com>
2024-03-27 05:56:01 -0700

    changes

--- main.c
+++ main.c
@@ -5,8 +5,16 @@
  * Return: 0
  */
 
-int main (void){
+int main (void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	int Vread;
 
-
+	printf("Enter value: \n");
+	Vread = getline(&line, &len, stdin);
+	if (Vread == -1)
+		printf("ERROR");
+	printf("%s", line);
 	return 0;
 }
