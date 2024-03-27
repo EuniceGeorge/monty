8233e2ebecd6b8df065f06b06184cc253e093ea6
Eunice George <georgeunice408@gmail.com>
2024-03-27 04:44:13 -0700

    Monty Project

---
+++ pall.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * f_pall - A function that prints all the arguments
+ * in the stack.
+ *
+ * @stack: The pointer to a pointer of the
+ * linkedlist
+ * @line_number: the line number of the file
+ * Return: void
+ */
+
+void f_pall(stack_t **stack, unsigned int line_number)
+{
+}
